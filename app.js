9f46764445b7cee90f724501afb96aa76082f676
Joseph Diable <aaron@josephs-mbp.home>
2020-05-07 13:16:47 +0100

    Adding interactive frontend UI, well playing around with the idea while learding design

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
-const express = require('express')
-const app = express()
-const cors = require('cors')
-const PORT = 8081
-const rootRouter = require('./router/rootRouter')
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const PORT = 8081;
+const rootRouter = require('./router/rootRouter');
+const membersRouter = require('./router/membersRouter');
+
+const cookieSession = require('cookie-session');
+
 const server = app.listen(PORT, () => console.log(`Server running on ${PORT}`))
 
 const corsOptions = {
@@ -15,8 +19,15 @@ app.use([
     express.json(),
     express.urlencoded({extended: true}),
     express.static('public'),
-    cors(corsOptions)
+    cors(corsOptions),
+    cookieSession({
+        name: 'ADsession',
+        secret: 'this is very specical, hide the real key!',
+              // Cookie Options
+        maxAge: 60 * 60 * 1000 // 1 hour??
+      })
 ])
 
 
 app.use('/', rootRouter);
+app.use('/members', membersRouter);
--- router/rootRouter.js
+++ router/rootRouter.js
@@ -4,27 +4,55 @@ let users = [
     {
         username: 'Aaron',
         email: 'aaron@gmail.com',
-        password: 'password'
-    }
+        password: 'password',
+        loggedIn: false
+    },
+    {
+        username: 'Bob',
+        email: 'bob@gmail.com',
+        password: 'password',
+        loggedIn: false
+    },
 ]
 
 router.get('/', (req, res) => {
-    users.forEach((item) => {console.log(item)})
-    res.render('index');
+    res.render('index', {loggedIn: req.session.profile ? true : false});
 });
 
 router.get('/json', (req, res) => {
     res.json(users);
 });
 
+router.get('/logout/user', (req, res) => {
+    req.session = null;
+    res.redirect('/')
+})
+
 router.post('/register/user', (req, res) => {
-    users.push({username: req.body.username, email: req.body.email, password: req.body.password})
+    users.push({username: req.body.username, email: req.body.email, password: req.body.password, loggedIn: false})
     res.redirect('/');;
 });
 
 router.post('/login/user', (req, res) => {
 
-    res.redirect('/');
+    let foundUser = users.forEach((item) => {
+        if (item.email === req.body.email) {
+                req.session.profile = {
+                    username: item.username,
+                    email: item.email,
+                    ID: req.sessionID,
+                    loggedIn: true
+                }
+                return true
+            }
+            
+        })
+
+
+            res.redirect('/members')
+
 });
+
+
 
 module.exports = router;
