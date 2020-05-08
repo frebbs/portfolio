298479af63f7abf0b3ef63575f7873104b653cfb
Joseph Diable <aaron@Josephs-MacBook-Pro.local>
2020-05-08 14:29:52 +0100

    changes

--- app.js
+++ app.js
@@ -9,17 +9,13 @@ const cookieSession = require('cookie-session');
 
 const server = app.listen(PORT, () => console.log(`Server running on ${PORT}`))
 
-const corsOptions = {
-    origin: 'http://localhost:8080',
-    optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
-}
 app.set('view engine', 'ejs')
 
 app.use([
     express.json(),
     express.urlencoded({extended: true}),
     express.static('public'),
-    cors(corsOptions),
+    cors(),
     cookieSession({
         name: 'ADsession',
         secret: 'this is very specical, hide the real key!',
