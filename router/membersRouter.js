955ccc732d58c526ab784d1ac9aafc92f9c934a1
Joseph Diable <aaron@Josephs-MacBook-Pro.local>
2020-05-09 14:36:01 +0100

    Fixed css

--- router/membersRouter.js
+++ router/membersRouter.js
@@ -2,8 +2,6 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-    console.log(req.session.userData)
-
     res.render('members', {session: req.session.userData.profile})
 })
 
